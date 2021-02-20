7b800d05c53bac353fc92230e898db0e0086ce22
Brandon Hilkert <brandonhilkert@gmail.com>
2021-02-20 13:16:36 -0800

    Faster triggering test

--- test.rb
+++ test.rb
@@ -9,9 +9,7 @@ RPi::GPIO.setup(PIN_NUM, as: :output)
 RPi::GPIO.set_high(PIN_NUM)
 
 RPi::GPIO.set_low(PIN_NUM)
-sleep 1
 RPi::GPIO.set_high(PIN_NUM)
-sleep 1
+sleep 3
 RPi::GPIO.set_low(PIN_NUM)
-sleep 1
 RPi::GPIO.set_high(PIN_NUM)
